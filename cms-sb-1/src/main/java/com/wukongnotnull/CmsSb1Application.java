ae11c5815680ef6c3469aac98848538764cb7d8a
wukong <wukong@wukong.lan>
2022-07-11 14:27:05 +0800

    first commit

---
+++ cms-sb-1/src/main/java/com/wukongnotnull/CmsSb1Application.java
@@ -0,0 +1,13 @@
+package com.wukongnotnull;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CmsSb1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CmsSb1Application.class, args);
+    }
+
+}
---
+++ cms-sb-1/src/test/java/com/wukongnotnull/CmsSb1ApplicationTests.java
@@ -0,0 +1,15 @@
+package com.wukongnotnull;
+
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CmsSb1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
